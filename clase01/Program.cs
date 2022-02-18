3a3bf1ce684f27baf7b142521e4246a4aaba8232
AlejandroSeku <99819999+AlejandroSeku@users.noreply.github.com>
2022-02-18 08:29:34 -0500

    Add files via upload

---
+++ clase01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HolaAña
+{
+ class Program
+ {
+     static void Main(String[] args)
+     {
+         int numero1, numero2, suma, producto;
+
+         Console.WriteLine("Suma y producto en dos números");
+
+         Console.WriteLine("Digite el primer número");
+         numero1=int.Parse(Console.ReadLine());
+
+         Console.WriteLine("Digite el segundo número");         
+         numero2=int.Parse(Console.ReadLine());
+
+         suma=numero1+numero2;
+
+         Console.WriteLine("La suma es " + suma);
+
+     }
+ }   
+}
---
+++ operadores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace operadores
+{
+ class Program
+ {
+     static void Main(String[] args)
+    {
+         float area, lado, radio;
+         string opcion = "";
+
+         while (opcion != "Circulo" || opcion != "Triangulo" || opcion != "Cuadrado")
+           {
+              Console.WriteLine("Ingrese la figura que desea encontrar el area (Circulo, Triangulo, Cuadrado)");
+              opcion = Console.ReadLine();
+
+                if (opcion=="Cuadrado")
+                {
+                    
+                    Console.WriteLine("El valor del lado");
+                    lado= float.Parse(Console.ReadLine());
+                    lado = lado * lado;
+                    Console.WriteLine("El area es " + lado);
+                 }
+                 else if(opcion=="Triangulo"){
+
+                    Console.WriteLine("El valor del lado");
+                    lado= float.Parse(Console.ReadLine());
+                    lado = lado * lado * lado;
+                    Console.WriteLine("El area es " + lado);
+                }
+                else if (opcion=="Circulo"){
+                    Console.WriteLine("El valor del radio");
+                    radio= float.Parse(Console.ReadLine());
+                    radio = radio *Math.PI;
+                    Console.WriteLine("El area es " + radio);
+                }
+                
+            }
+
+        }
+        
+ }
+}
